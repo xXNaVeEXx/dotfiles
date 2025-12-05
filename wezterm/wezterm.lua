7318b9c17f5d48f60f39f8ec1d0e6eb839f0032d
Nave <naveone1@gmail.com>
2025-12-05 01:48:55 +0100

    font size wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,18 +5,18 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font('GohuFont 14 Nerd Font Mono')
-config.font_size = 14
+config.font_size = 19
 
 config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+  foreground = "#CBE0F0",
+  background = "#011423",
+  cursor_bg = "#47FF9C",
+  cursor_border = "#47FF9C",
+  cursor_fg = "#011423",
+  selection_bg = "#033259",
+  selection_fg = "#CBE0F0",
+  ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+  brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
 config.enable_tab_bar = false
@@ -60,4 +60,3 @@ config.mouse_bindings = {
 
 -- and finally, return the configuration to wezterm
 return config
-
